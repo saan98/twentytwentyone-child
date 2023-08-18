04f521c2d610c12bbb1602796cc0f47b6a546d3a
Ishan Shrestha <ishanstha1998@gmail.com>
2023-08-18 18:31:14 +0545

    b4-after

--- functions.php
+++ functions.php
@@ -6,11 +6,12 @@ function child_enqueue_styles() {
 	wp_enqueue_style( 'child-theme', get_stylesheet_directory_uri() . '/style.css', array(), 100 );
 }
 
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' ); // Remove the // from the beginning of this line if you want the child theme style.css file to load on the front end of your site.
+//add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' ); // Remove the // from the beginning of this line if you want the child theme style.css file to load on the front end of your site.
 
 function tto_scripts() {
 	wp_enqueue_style( 'custom-css', get_stylesheet_directory_uri(). '/assets/css/mystyle.css' );
 	wp_enqueue_style( 'bootstrap-css', get_stylesheet_directory_uri(). '/assets/css/bootstrap.min.css' );
+	wp_enqueue_style( 'qodesocial-css', get_stylesheet_directory_uri(). '/assets/css/qodesocial.css' );
 	wp_enqueue_style( 'owl-carousel-css', get_stylesheet_directory_uri(). '/assets/owl_carousel/owl.carousel.css' );
 	wp_enqueue_style( 'owl-theme-default-css', get_stylesheet_directory_uri(). '/assets/owl_carousel/owl.theme.default.css' );
 
--- page-templates/template-qodesocial.php
+++ page-templates/template-qodesocial.php
@@ -3,7 +3,7 @@
 <?php get_header() ?>
 
 <section class="main-banner">
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-lg-6 col-sm-12">
                 <h2 class="banner-title">Qode Social</h2>
@@ -12,8 +12,8 @@
                 <a name="" id="" class="btn" href="#" role="button">Our Work</a>
             </div>
             <div class="col-lg-6 col-sm-12">
-                <div>
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/inner-page/Hand.png"
+                <div class="banner-background">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/inner-page/banner-hand.png"
                         class="img-fluid rounded-top" alt="a hand holding phone">
                 </div>
             </div>
@@ -28,35 +28,30 @@
                             src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/inner-page/Facebook.png">
                     </div>
                 </div>
-
                 <div class="col-md-2  col-sm-4 col-6">
                     <div classs="logo">
                         <img class="scorecard-report-img"
                             src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/inner-page/Instagram.png">
                     </div>
                 </div>
-
-                <div class=" col-md-2 col-sm-4 col-6">
-                    <div classs="logo">
-                        <img class="scorecard-report-img"
-                            src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/inner-page/Google-Ads.png">
-                    </div>
-                </div>
-
-                <div class="col-md-2  col-sm-4 col-6">
-                    <div classs="logo google-logo">
-                        <img class="scorecard-report-img"
-                            src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/inner-page/Google-Partners.png">
-                    </div>
-                </div>
-
                 <div class="col-md-2  col-sm-4 col-6">
                     <div classs="logo">
                         <img class="scorecard-report-img"
                             src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/inner-page/Hootsuite.png">
                     </div>
                 </div>
-
+                <div class=" col-md-2 col-sm-4 col-6">
+                    <div classs="logo">
+                        <img class="scorecard-report-img"
+                            src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/inner-page/Google-Ads.png">
+                    </div>
+                </div>
+                <div class="col-md-2  col-sm-4 col-6">
+                    <div classs="logo google-logo">
+                        <img class="scorecard-report-img"
+                            src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/inner-page/Google-Partners.png">
+                    </div>
+                </div>
                 <div class="col-md-2  col-sm-4 col-6">
                     <div classs="logo">
                         <img class="scorecard-report-img"
@@ -67,6 +62,51 @@
         </div>
     </div>
 </section>
+
+<!-- ========== Start Food-Banner ========== -->
+<section class="food-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-sm-12">
+                <div class="banner-background">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/inner-page/food.png"
+                        class="img-fluid rounded-top" alt="a hand holding phone">
+                </div>
+            </div>
+            <div class="col-lg-6 col-sm-12">
+                <h2 class="banner-title">Social media agency for businesses in Toronto</h2>
+                <p>Full service social media marketing agency & content creation agency</p>
+                <a name="" id="" class="btn" href="#" role="button">About Us</a>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<!-- ========== End Food-Banner ========== -->
+
+<!-- ========== Start Service Section ========== -->
+
+<section class="service-section">
+    <div class="container">
+        <div class="row-cols-4 justify-content-center align-items-center g-2">
+            <div class="col-lg-6">
+                <div class="col-lg-6 col-sm-12">
+                    <p>Here’s a highlight of some of the services that set us apart as Toronto Social Media Experts</p>
+                    <a name="" id="" class="btn" href="#" role="button">View All Services</a>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="row">
+                    <div class="col-4"></div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<!-- ========== End Service Section ========== -->
 
 
 <?php get_footer() ?>
