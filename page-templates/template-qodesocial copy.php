397ce3be2b9eac4f0ecf2b15cee0b9e0b1d958a1
saan98 <ishanstha1998@gmail.com>
2023-08-22 00:16:04 +0530

    sss

--- page-templates/template-qodesocial copy.php
+++ page-templates/template-qodesocial copy.php
@@ -5,20 +5,15 @@
     <!-- ========== Start Banner ========== -->
     <section class="bannerz">
         <div class="container">
-            <div class="row">
+            <div class="row justify-content-center align-items-center">
                 <div class="col-lg-6 col-sm-12">
                     <p class="titlez">Qode Social</p>
                     <p>Uniting Brands with Audiences®</p>
-                    <div class="button-with-text-and-icon99">
-                        <div class="container59">
-                            <b class="button-text9">Let’s Get Started</b>
-                            <div class="icon59"><img
-                                    src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/public/arrow-right.png" />
-                            </div>
-                        </div>
+                    <div class="main-btnz">
+                        <a name="" id="" class="btn btn-primary" href="#" role="button">Let's Get Started</a>
                     </div>
-                    <div class="btn9">
-                        <a name="" id="" class="btn" href="#" role="button">Our Work</a>
+                    <div class="main-btnz">
+                        <a name="" id="" class="btn btn-light" href="#" role="button">Our Work</a>
                     </div>
                 </div>
                 <div class="col-lg-6 col-sm-12">
@@ -35,7 +30,7 @@
     <!-- ========== Start Badges ========== -->
     <section class="badgesz">
         <div class="container">
-            <div class="row">
+            <div class="row justify-content-center align-items-center">
                 <div class="col-lg-2 col-sm-12">
                     <img class="" alt=""
                         src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/inner-page/Facebook.png" />
@@ -69,7 +64,7 @@
     <!-- ========== Start Intro ========== -->
     <section class="introz">
         <div class="container">
-            <div class="row">
+            <div class="row justify-content-center align-items-center">
                 <div class="col-lg-6 col-sm-12">
                     food-banner
                 </div>
@@ -82,6 +77,113 @@
         </div>
     </section>
     <!-- ========== End Intro ========== -->
+
+    <!-- ========== Start Services ========== -->
+    <section class="servicesz">
+        <div class="container">
+            <div class="row justify-content-center align-items-center justify-content-center align-items-center">
+                <div class="col-lg-6 col-sm-12">
+                    <p class="textz">
+                        Here’s a highlight of some of the services that set us apart as Toronto Social Media Experts
+                    </p>
+                    <div class="main-btn-design">
+                        <a name="" id="" class="btn btn-primary" href="#" role="button">View All Services</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-12">
+                    <div class="row justify-content-center align-items-center gap-5">
+                        <div class="service-iconz-div col-lg-4">
+                            <img class="service-iconz" alt="Social Copywriting"
+                                src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/public/service-iconmonthly-content-calendars.svg" />
+                        </div>
+                        <div class="service-iconz-div col-lg-4">
+                            <img class="service-iconz" alt="Monthly Content Calendars"
+                                src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/public/service-iconsocial-copywriting.svg" />
+                        </div>
+                        <div class="service-iconz-div col-lg-4">
+                            <img class="service-iconz" alt="Content & Visual Strategy"
+                                src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/public/service-iconcontent--visual-strategy.svg" />
+                        </div>
+                    </div>
+                    <div class="row justify-content-center align-items-center gap-5">
+                        <div class="service-iconz-div col-lg-4">
+                            <img class="service-iconz" alt="Videography"
+                                src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/public/service-iconvideography.svg" />
+                        </div>
+                        <div class="service-iconz-div col-lg-4">
+                            <img class="service-iconz" alt="Photography"
+                                src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/public/service-iconphotography.svg" />
+                        </div>
+                        <div class="service-iconz-div col-lg-4">
+                            <img class="service-iconz" alt="Analytic Reporting"
+                                src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/public/service-iconanalytic-reporting.svg" />
+                        </div>
+                    </div>
+                    <div class="row justify-content-center align-items-center gap-5">
+                        <div class="service-iconz-div col-lg-4">
+                            <img class="service-iconz" alt="Market Research"
+                                src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/public/service-iconmarket-research.svg" />
+                        </div>
+                        <div class="service-iconz-div col-lg-4">
+                            <img class="service-iconz" alt="Social Media Management"
+                                src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/public/service-iconsocial-media-management.svg" />
+                        </div>
+                        <div class="service-iconz-div col-lg-4">
+                            <img class="service-iconz" alt="Conversion Campaigns"
+                                src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/public/service-iconconversion-campaigns.svg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    <!-- ========== End Services ========== -->
+
+    <!-- ========== Start Case Study ========== -->
+    <section class="case-studyz">
+        <div class="container">
+            <div class="row text-center align-items-center">
+                <div class="col-lg-4 col-sm-12">
+                    <div class="main-btnz">
+                        <a name="" id="" class="btn btn-secondary" href="#" role="button">Button</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-12">
+                    <div class="main-btnz">
+                        <a name="" id="" class="btn btn-secondary" href="#" role="button">Button</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-12">
+                    <div class="main-btnz">
+                        <a name="" id="" class="btn btn-secondary" href="#" role="button">Button</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ========== End Case Study ========== -->
+
+    <!-- ========== Start Content Creation ========== -->
+    <section class="content-creationz">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-sm-12">
+                    picture
+                </div>
+                <div class="col-lg-6 col-sm-12">
+                    <p>The Qode Social team is amazing to work with. Highly responsive and professional. They take the
+                        time to understand your business and position for market dominance through highly polished
+                        digital assets. I wouldn’t trust my businesses with anyone else.</p>
+                    <p>
+                        Leslie
+                        Toronto Business Owner
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ========== End Content Creation ========== -->
 
 </main>
 <?php get_footer() ?>
